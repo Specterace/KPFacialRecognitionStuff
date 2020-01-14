ae06a6fce683cc1b054424eaf7878245194d40ba
Oscar <ochoy@mail.ccsf.edu>
2020-01-14 01:15:12 -0800

    Update facialNNCodeNew.py

--- facialNNCodeNew.py
+++ facialNNCodeNew.py
@@ -1,15 +1,4 @@
-# example of loading the keras facenet model
-from keras.models import load_model
-# load the model
-model = load_model('facenet_keras.h5')
-# summarize input and output shape
-#print(model.inputs)
-#print(model.outputs)
-print('keras model loaded')
-import mtcnn
-
-#print(mtcnn.__version__)
-#print('mtcnn loaded')
+#facial recognition code
 
 #various imports
 from os import listdir
@@ -17,17 +6,24 @@ from os.path import isdir
 from PIL import Image
 from numpy import asarray
 from numpy import savez_compressed
-from matplotlib import pyplot
-from mtcnn.mtcnn import MTCNN
-
 from numpy import load
 from numpy import expand_dims
-
+from matplotlib import pyplot
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import Normalizer
 from sklearn.svm import SVC
 from random import choice
+
+from keras.models import load_model
+# load the model
+model = load_model('facenet_keras.h5')
+print('keras model loaded')
+
+import mtcnn
+from mtcnn.mtcnn import MTCNN
+#print(mtcnn.__version__)
+#print('mtcnn loaded')
 
 #function to extract a single face from a given photograph
 def extract_face(filename, required_size=(160, 160)):
@@ -54,25 +50,20 @@ def extract_face(filename, required_size=(160, 160)):
     face_array = asarray(image)
     return face_array
 
-#specify folder to plot
-#folder = '5-celebrity-faces-dataset/train/ben_affleck/'
-#i = 1
-#enumerate files
-
 def load_faces(directory):
     faces = list()
     #enumerate files
     for filename in listdir(directory):
+      if (filename == '.DS_Store'):
+        continue
+      else:
         #path
         path = directory + filename
+        ##########printing path for debugging purposes##########
+        print(path)
+        ########################################################
         #get face
         face = extract_face(path)
-        #print(i, face.shape)
-        #plot
-        #pyplot.subplot(2, 7, i)
-        #pyplot.axis('off')
-        #pyplot.imshow(face)
-        #i += 1
         #store
         faces.append(face)
     return faces
@@ -89,7 +80,7 @@ def load_dataset(directory):
             continue
         #load all faces in the subdirectory
         faces = load_faces(path)
-        #create labels
+        #create labels (this gives a picture the label of it's subdirectory's name)
         labels = [subdir for _ in range(len(faces))]
         #summarize progress
         print('>loaded %d examples for class: %s' % (len(faces), subdir))
@@ -112,19 +103,25 @@ def get_embedding(model, face_pixels):
     yhat = model.predict(samples)
     return yhat[0]
 
+#########loading and extracting faces from images
 #load train dataset
-trainX, trainy = load_dataset('5-celebrity-faces-dataset/train/')
-print(trainX.shape, trainy.shape)
+#############REPLACE THE '5-celebrity-faces-dataset' with the training set needed
+trainX, trainy = load_dataset('8-celebrity-faces-dataset/train/')
+#print(trainX.shape, trainy.shape)
 #load test dataset
-testX, testy = load_dataset('5-celebrity-faces-dataset/val/')
-print(testX.shape, testy.shape)
+#############REPLACE THE '5-celebrity-faces-dataset' with the training set needed
+testX, testy = load_dataset('8-celebrity-faces-dataset/val/')
+#print(testX.shape, testy.shape)
 #save arrays to one file in compressed format
 #savez_compressed('5-celebrity-faces-dataset.npz', trainX, trainy, testX, testy)
-savez_compressed('5-celebrity-faces-dataset-new.npz', trainX, trainy, testX, testy)
+savez_compressed('8-celebrity-faces-dataset.npz', trainX, trainy, testX, testy)
+print('file saved')
 
+#######converting extracted faces into embeddings
 #load the face load_dataset
 #this set is presaved in an ".npz" file type
-data = load('5-celebrity-faces-dataset.npz')
+#this name needs to match with the filename of the faces extracted save file ".npz"
+data = load('8-celebrity-faces-dataset.npz')
 trainX, trainy, testX, testy = data['arr_0'], data['arr_1'], data['arr_2'], data['arr_3']
 print('Loaded: ', trainX.shape, trainy.shape, testX.shape, testy.shape)
 #load the facenet model
@@ -146,26 +143,30 @@ newTestX = asarray(newTestX)
 print(newTestX.shape)
 #save arrays to one file in compressed format
 #savez_compressed('5-celebrity-faces-embeddings.npz', newTrainX, trainy, newTestX, testy)
-savez_compressed('5-celebrity-faces-embeddings-new.npz', newTrainX, trainy, newTestX, testy)
+savez_compressed('8-celebrity-faces-embeddings.npz', newTrainX, trainy, newTestX, testy)
 
+############Deploying a model to classify face embeddings##############
+#####Creating labelling for the faces passed and loaded in################
 #load dataset
 #data = load('5-celebrity-faces-embeddings.npz')
-ata = load('5-celebrity-faces-embeddings-new.npz')
+data = load('8-celebrity-faces-embeddings.npz')
 trainX, trainy, testX, testy = data['arr_0'], data['arr_1'], data['arr_2'], data['arr_3']
 print('Dataset: train=%d, text=%d' % (trainX.shape[0], testX.shape[0]))
-#normalize input vectors
+#normalize input vectors (the face embedding vectors)
 in_encoder = Normalizer(norm='l2')
 trainX = in_encoder.transform(trainX)
 tesX = in_encoder.transform(testX)
-#label encode targets
+#label encode targets (converting the string target variables to integers)
 out_encoder = LabelEncoder()
 out_encoder.fit(trainy)
 trainy = out_encoder.transform(trainy)
 testy = out_encoder.transform(testy)
 
+############fitting the model/linear SVM with the training data encoded#################
 #fit model
 model = SVC(kernel='linear', probability=True)
 model.fit(trainX, trainy)
+###evaluating the model##########
 #predict
 yhat_train = model.predict(trainX)
 yhat_test = model.predict(testX)
@@ -175,11 +176,13 @@ score_test = accuracy_score(testy, yhat_test)
 #summarize
 print('Accuracy: train=%.3f, test=%.3f' % (score_train*100, score_test*100))
 
+##########plotting original face and the prediction#############
+#fitting the model with the test data loaded in############
 #load faces
-data = load('5-celebrity-faces-dataset-new.npz')
+data = load('8-celebrity-faces-dataset.npz')
 testX_faces = data['arr_2']
 # load face embeddings
-data = load('5-celebrity-faces-embeddings-new.npz')
+data = load('8-celebrity-faces-embeddings.npz')
 trainX, trainy, testX, testy = data['arr_0'], data['arr_1'], data['arr_2'], data['arr_3']
 # normalize input vectors
 in_encoder = Normalizer(norm='l2')
@@ -193,12 +196,18 @@ testy = out_encoder.transform(testy)
 # fit model
 model = SVC(kernel='linear', probability=True)
 model.fit(trainX, trainy)
+
 #test model on a random example from the test Dataset
+#if wanted, can modify code here to take a random image and evaluate it#####
 selection = choice([i for i in range(testX.shape[0])])
 random_face_pixels = testX_faces[selection]
 random_face_emb = testX[selection]
 random_face_class = testy[selection]
 random_face_name = out_encoder.inverse_transform([random_face_class])
+
+#take an image, extract a face, get it's embedding, store it's class/name
+#if it is another image, no need to encode it (not yet)
+
 # prediction for the face
 samples = expand_dims(random_face_emb, axis=0)
 yhat_class = model.predict(samples)
